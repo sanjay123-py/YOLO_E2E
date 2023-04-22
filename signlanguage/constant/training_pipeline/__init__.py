114061ee6e6391b8d5796927c8a01b9db8324b93
sanjay123-py <sanjaysanjay1270@gmail.com>
2023-04-22 14:21:57 +0530

    data validation completed

--- signlanguage/constant/training_pipeline/__init__.py
+++ signlanguage/constant/training_pipeline/__init__.py
@@ -1,13 +1,14 @@
 import os
 
+#Data Ingestion constants
+
 ARTIFACTS_DIR: str = "artifacts"
-
-
-"""
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
-"""
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
-
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
-
 DATA_DOWNLOAD_URL: str = "https://github.com/entbappy/Branching-tutorial/raw/master/Sign_language_data.zip"
+
+#Data Validation constants
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_STATUS_FILE = "status.txt"
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "data.yml"]
--- signlanguage/pipeline/training_pipeline.py
+++ signlanguage/pipeline/training_pipeline.py
@@ -3,13 +3,15 @@ import sys
 from signlanguage.exception import SignException
 from signlanguage.logger import logging
 from signlanguage.components.data_ingestion import DataIngestion
-from signlanguage.entity.config_entity import DataIngestionConfig
-from signlanguage.entity.artifact_entity import DataIngestionArtifact
+from signlanguage.components.data_validation import DataValidation
+from signlanguage.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from signlanguage.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -22,9 +24,22 @@ class TrainingPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise SignException(e,sys)
+
+    def start_data_validation(self, data_ingestion_artifact:DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info(f"{'#'*10} Entered the data validation pipeline {'#'*10}")
+        logging.info("Getting the data from url")
+        try:
+            data_validation = DataValidation(data_ingestion_artifact, self.data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except Exception as e:
+            raise SignException(e,sys)
+
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            return data_ingestion_artifact
+            data_validation_artifact=self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
         except Exception as e:
             raise SignException(e, sys)
